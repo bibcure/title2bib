32832f5a0260df93ca38b0e043685583d6d931e5
bruno_messias <messias.physics@gmail.com>
2017-08-12 18:41:02 -0300

    Fix wrong setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ readme.close()
 
 setup(
     name="titletobib",
-    version="0.1",
+    version="0.2",
     packages = find_packages(exclude=["build",]),
     scripts=["titletobib/bin/titletobib"],
     long_description = README_TEXT,
@@ -16,7 +16,7 @@ setup(
     description="Generate a bibtex given a title",
     author="Bruno Messias",
     author_email="messias.physics@gmail.com",
-    download_url="https://github.com/bibcure/titletobib/archive/0.1.tar.gz",
+    download_url="https://github.com/bibcure/titletobib/archive/0.2.tar.gz",
     keywords=["bibtex", "science","scientific-journals"],
 
     classifiers=[
