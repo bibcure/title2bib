2cba886e771e73455ffa68b942e207a3cce42f20
bruno_messias <messias.physics@gmail.com>
2017-08-20 16:07:16 -0300

    Now, arxiv database also integrated

--- setup.py
+++ setup.py
@@ -6,21 +6,20 @@ readme.close()
 
 setup(
     name="titletobib",
-    version="0.2.2",
+    version="0.2.3",
     packages = find_packages(exclude=["build",]),
     scripts=["titletobib/bin/titletobib"],
     long_description = README_TEXT,
-    install_requires=["requests", "future", "doitobib"],
+    install_requires=["requests", "future", "doitobib","arxivcheck"],
     include_package_data=True,
     license="GPLv3",
     description="Generate a bibtex given a title",
     author="Bruno Messias",
     author_email="messias.physics@gmail.com",
-    download_url="https://github.com/bibcure/titletobib/archive/0.2.2.tar.gz",
+    download_url="https://github.com/bibcure/titletobib/archive/0.2.3.tar.gz",
     keywords=["bibtex", "science","scientific-journals"],
 
     classifiers=[
-        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Intended Audience :: Science/Research",
         "Programming Language :: Python",
--- titletobib/crossref.py
+++ titletobib/crossref.py
@@ -3,6 +3,7 @@ import requests
 from builtins import input
 import difflib
 from doitobib.crossref import get_bib_from_doi
+from arxivcheck.arxiv import get_arxiv_info, generate_bib_from_arxiv
 bare_url = "http://api.crossref.org/"
 
 
@@ -18,7 +19,7 @@ def ask_which_is(title, items):
     question = "\t It is >>'{}' article?y(yes)|n(no)|q(quit)"
     for item in items:
         w = input(question.format(
-            item["title"][0].encode("ascii", "ignore"), title))
+            item["title"], title))
         if w == "y":
             found = True
             result = item
@@ -33,7 +34,7 @@ def sort_items_by_title(items, title):
         items,
         key=lambda x: difflib.SequenceMatcher(
             None,
-            x["title"][0],
+            x["title"],
             title
         ).ratio(),
         reverse=True
@@ -45,6 +46,13 @@ def get_from_title(title, get_first=False):
     params = {"query.title": title, "rows": 10}
     r = find_cross_info(params)
     items = r.json()["message"]["items"]
+    for i, item in enumerate(items):
+        items[i]["title"] = item["title"][0].encode("ascii", "ignore")
+        items[i]["is_crossref"] = True
+    found_arxiv,  arxiv_items = get_arxiv_info(title, field="ti")
+    for arxiv_item in arxiv_items:
+        arxiv_item["is_crossref"] = False
+        items.append(arxiv_item)
     if r.status_code == 200 and len(items) > 0:
         items = sort_items_by_title(items, title)
         if get_first:
@@ -61,14 +69,19 @@ def get_bib_from_title(title, get_first=False, abbrev_journal=False):
     bib = ""
     found, item = get_from_title(title, get_first)
     if found:
-        if "DOI" in item:
-            doi = item["DOI"]##aqui pode acontecer de realizar a chamada para
-            found, bib = get_bib_from_doi(doi)
-            if "short-container-title" in item:
-                abbreviated_journal = item["short-container-title"][0]##aqui pode acontecer de realizar a chamada para
+        if item["is_crossref"]:
+            if "DOI" in item:
+                doi = item["DOI"]
+                found, bib = get_bib_from_doi(doi)
 
-        #pegar journal contraido e contrair autores
-        # depois fazer um replace no bib com  o nome do journal e o
-        #contraido, ou usar o bibtexparser(ultima melhor)
+        else:
+            if "arxiv_doi" in item:
+                doi = item["arxiv_doi"]
+                published, bib = get_bib_from_doi(doi, abbrev_journal)
+            else:
+                bib = generate_bib_from_arxiv(item, title, field="ti")
+
+            # if "short-container-title" in item:
+            # abbreviated_journal = item["short-container-title"][0]##aqui pode acontecer de realizar a chamada para
 
     return found, bib
